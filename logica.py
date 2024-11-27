5f8fb9a15330b8c2bc461fc1b080ede53d19f43f
Ademir Vieira <vieiraademir707@gmail.com>
2024-11-27 15:00:13 -0300

    parte de exercicios

--- logica.py
+++ logica.py
@@ -1,3 +1,16 @@
+from string import ascii_letters, digits, punctuation
+import random as rn
+
+# Conjunto de caracteres disponíveis
+characters = ascii_letters + digits + punctuation
+
+# Tamanho da senha
+password_length = 8
+
+# Calcula o número total de combinações possíveis
+total_combinations = len(characters) ** password_length
+print(f"Total de combinações possíveis: {total_combinations}")
+
 
 if __name__ == '__main__':
     def soma(lista):
@@ -9,5 +22,19 @@ if __name__ == '__main__':
         frase = frase.replace(" ","").lower()
         return frase == frase[::-1]
 print(palindro("A base do teto desaba"))
+
+print(rn.randrange(1,10))
+class perso:
+    def __init__(car, age, name):
+        car.age = age
+        car.name = name
+        
+p1 = perso(43, "Rzober")
+print(p1.age)
+print(p1.name)
+
+
+
+
 
 
